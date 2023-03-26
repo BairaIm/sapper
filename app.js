46250b7f9ac6f3da5b28c4198ee9694df03db265
Baira <imeevaba@mail.ru>
2023-03-26 22:38:10 +0300

    Random fixed, all pictures replaced with sprites, checkbox mode changed in mobile version

--- app.js
+++ app.js
@@ -5,27 +5,73 @@ let WIDTH = 16,
     HEIGHT = 16,
     BOMBS_COUNT = 40;
 
+const hintBtn = document.querySelector('.hint');
+let cntHints = 0;
+
+const ownLevel = document.querySelector('.own');
+const ownBtn = document.querySelector('.own-btn');
+const ownLevelWidth = ownLevel.querySelector('#width');
+const ownLevelHeight = ownLevel.querySelector('#height');
+const ownLevelBombs = ownLevel.querySelector('#bombs');
+
 document.querySelectorAll('input[name="level"]').forEach(radioBtn => 
             radioBtn.addEventListener('change', 
             () => {
                 if (radioBtn.value == 1) {
-                    WIDTH = 8,
-                    HEIGHT = 8,
-                    BOMBS_COUNT = 10
+                    WIDTH = 8;
+                    HEIGHT = 8;
+                    BOMBS_COUNT = 10;
+
+                    hintBtn.addEventListener('click', getHint);
+                    hintBtn.classList.remove('hidden');
+                    cntHints = 3;
+
+                    ownLevel.classList.add('hidden');
+                    detachOwnLevel();
+
+                    startGame();
                 } else if (radioBtn.value == 2) {
-                    WIDTH = 12,
-                    HEIGHT = 12,
-                    BOMBS_COUNT = 20
+                    WIDTH = 12;
+                    HEIGHT = 12;
+                    BOMBS_COUNT = 20;
+
+                    hintBtn.removeEventListener('click', getHint);
+                    hintBtn.classList.add('hidden');
+
+                    ownLevel.classList.add('hidden');
+                    detachOwnLevel();
+
+                    startGame();
                 } else if (radioBtn.value == 3) {
-                    WIDTH = 16,
-                    HEIGHT = 16,
-                    BOMBS_COUNT = 40
+                    WIDTH = 16;
+                    HEIGHT = 16;
+                    BOMBS_COUNT = 40;
+
+                    hintBtn.removeEventListener('click', getHint);
+                    hintBtn.classList.add('hidden');
+
+                    ownLevel.classList.add('hidden');
+                    detachOwnLevel();
+
+                    startGame();
+                } else {
+                    WIDTH = 0;
+                    HEIGHT = 0;
+                    BOMBS_COUNT = 0;
+
+                    ownLevel.classList.remove('hidden');
+
+                    hintBtn.removeEventListener('click', getHint);
+                    hintBtn.classList.add('hidden');
+                    attachOwnLevel();
                 }
-                startGame();
             }));
     
 const smile = document.querySelector('.smile');
 smile.addEventListener('click', () => {
+    resetType();
+    toggleTypeOne.disabled = false;
+    toggleTypeSet.disabled = false;
     startGame();
 });
 
@@ -33,6 +79,11 @@ const field = document.querySelector('.field');
 field.addEventListener('contextmenu', event => event.preventDefault());
 let cells;
 
+
+const digits = document.querySelectorAll('.digit');
+const game = document.querySelector('.game');
+const container = document.querySelector('.container');
+    
 const counter100 = document.querySelector('#counter-mines-100');
 const counter10 = document.querySelector('#counter-mines-10');
 const counter1 = document.querySelector('#counter-mines-1');
@@ -42,22 +93,25 @@ const timer100 = document.querySelector('#timer-100');
 const timer10 = document.querySelector('#timer-10');
 const timer1 = document.querySelector('#timer-1');
 
-let typeClick = document.querySelector('input[name="type-click"]:checked').value;
+let typeClick = "cell";
+
+const toggleTypeOne = document.querySelector('.flag-one');
+const toggleTypeSet = document.querySelector('.flag-set');
+
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
-    .test(navigator.userAgent)) {
-        document.querySelector('.type-click').classList.remove('hidden');
-    document.querySelectorAll('input[name="type-click"]').forEach(radioBtn => 
-            radioBtn.addEventListener('change', handleRadioTypeClick));
+        .test(navigator.userAgent)) {
+    document.querySelector('.type-click').classList.remove('hidden');
+
+    toggleTypeSet.addEventListener('click', toggleType);
+    toggleTypeOne.addEventListener('click', toggleTypeOneTime);
 }
 
 const texts = document.querySelectorAll('.text-2');
 
-let toggleType = document.querySelector('.toggle-type');
-toggleType.addEventListener('click', toggleTypeOneTime);
     
 let closeCount;
 let leftMines;
-let bombs;
+let bombs = [];
     
 
 startGame();
@@ -68,75 +122,155 @@ function startGame() {
         is_start = false;
     }
     
+    bombs = [];
+    
     closeCount = WIDTH * HEIGHT;
     leftMines = BOMBS_COUNT;
-    
-    bombs = [...Array(closeCount).keys()]
-        .sort(() => Math.random() - 0.5)
-        .slice(0, BOMBS_COUNT);
-    
-    const sizeCell = Number(document.querySelector('input[name="size_cell"]:checked').value);
 
     document.querySelectorAll('input[name="size_cell"]').forEach(radioBtn => 
             radioBtn.addEventListener('change', handleRadioSizeCell));
-    renderField(closeCount, sizeCell);
-    renderSize(sizeCell);
-
+    renderField(closeCount);
     renderCounter(BOMBS_COUNT);
     renderTimer(0);
     
-    // console.log(1, cells);
+    installationMines(BOMBS_COUNT);
+    
+    cntHints = 3;
+    hintBtn.disabled = false;
+    hintBtn.innerHTML = 'У вас 3 подсказки';
 };
 
-function toggleTypeOneTime() {
-    if (typeClick === 'flag') {
-        toggleType.classList.replace('flag_green', 'flag');
-        typeClick = 'cell';
+function getHint() {
+    const closedCellsWithoutMine = cells.filter((cell, index) => !bombs.includes(index) && cell.dataset.isOpen != 1);
+    
+    cntHints--;
+    if (cntHints <= 0) {
+        hintBtn.disabled = true;
+        hintBtn.innerHTML = 'У вас 0 подсказок';
     } else {
-        toggleType.classList.replace('flag', 'flag_green');
-        typeClick = 'flag';
+        cntHints == 1
+            ? hintBtn.innerHTML = 'У вас 1 подсказка'
+            : hintBtn.innerHTML = 'У вас 2 подсказки';
     }
+    
+    console.log(closedCellsWithoutMine);
+    const res = Math.floor(Math.random() * (closedCellsWithoutMine.length - 1));
+    const cell = closedCellsWithoutMine[res];
+    
+    if (cell.dataset.flag == 1) {
+        cell.dataset.flag = 0;
+        cell.classList.replace(cell.classList[2], 'flag_green');
+    } else {
+        cell.classList.replace(cell.classList[2], 'cell_green');
+    }
+}
+
+function installationMines(countInstallMines) {
+    let cnt = 0;
+    
+    while (cnt < countInstallMines) {
+        const index = Math.floor(Math.random() * (closeCount - 1));
+        if (!bombs.includes(index) && cells[index].dataset.isOpen == 0) {
+            bombs. push(index);
+            cnt++;
+        } 
+    }
+}
+
+function toggleTypeOneTime() {
+    toggleTypeSet.classList.replace('flag-set-press', 'flag-set');
+
+    if (typeClick != 'flag') {
+        toggleTypeOne.classList.replace('flag-one', 'flag-one-press');
+        typeClick = 'flag';
+    } else {
+        toggleTypeOne.classList.replace('flag-one-press', 'flag-one');
+        typeClick = 'cell';
+    }
+}
+
+function toggleType() {
+    toggleTypeOne.classList.replace('flag-one-press', 'flag-one');
+
+    if (typeClick != 'flags') {
+        typeClick = 'flags';
+        toggleTypeSet.classList.replace('flag-set', 'flag-set-press');
+    } else {
+        typeClick = 'cell';
+        toggleTypeSet.classList.replace('flag-set-press', 'flag-set');
+    }
+};
+
+function resetType() {
+    typeClick = 'cell';
+    toggleTypeSet.classList.replace('flag-set-press', 'flag-set');
+    toggleTypeOne.classList.replace('flag-one-press', 'flag-one');
 }
 
 function toggleFlag(index) {
     const cell = cells[index];
     
     if (cell.dataset.isOpen == 1) {
-        return;
+        return false;
     }
     
     if (cell.dataset.flag == 0) {
         leftMines--;
         renderCounter(leftMines);
-        // cell.innerHTML = '<img class="count_mines" src="img/flag.png" alt="count mines">';
         cell.classList.replace(cell.classList[2], 'flag');
     } else if (cell.dataset.flag == 1) {
         leftMines++;
         renderCounter(leftMines);
-        // cell.innerHTML = '<img class="count_mines" src="img/question.png" alt="count mines">';
         cell.classList.replace(cell.classList[2], 'question');
     } else {
-        // cell.innerHTML = '<img class="count_mines" src="img/closed_cell.png" alt="count mines">';
         cell.classList.replace(cell.classList[2], 'cell_closed');
     }
     cell.dataset.flag = cell.dataset.flag < 3 
         ? cell.dataset.flag + 1 
         : 0;
+    
+    return true;
 }
-function handleRadioSizeCell(event) {
-    renderSize(Number(event.target.value));
-};
 
-function handleRadioTypeClick(event) {
-    typeClick = document.querySelector('input[name="type-click"]:checked').value;
-    if (typeClick === 'flag') {
-        toggleType.disabled = true;
-        toggleType.classList.replace(toggleType.classList[0], 'flag_grey');
+function attachOwnLevel() {
+    ownLevelWidth.addEventListener('keypress', handleMoveNext);
+    ownLevelHeight.addEventListener('keypress', handleMoveNext);
+    ownLevelBombs.addEventListener('keypress', handleMoveNext);
+    ownBtn.addEventListener('click', handleStart);
+}
+
+function detachOwnLevel() {
+    ownLevelWidth.removeEventListener('keypress', handleMoveNext);
+    ownLevelHeight.removeEventListener('keypress', handleMoveNext);
+    ownLevelBombs.removeEventListener('keypress', handleMoveNext);
+    ownBtn.removeEventListener('click', handleStart);
+}
+
+function handleStart() {
+    const w = +ownLevelWidth.value;
+    const h = +ownLevelHeight.value;
+    const b = +ownLevelBombs.value;
+
+    if (isValidNumber(w) && isValidNumber(h) && isValidNumber(b) && b < w  * h) {
+        WIDTH = w;
+        HEIGHT = h;
+        BOMBS_COUNT = b;
     } else {
-        toggleType.disabled = false;
-        toggleType.classList.replace(toggleType.classList[0], 'flag');
+        alert('Width, height and number of bombs must be > 0. And number of bombs must be < weight * height')
     }
-};
+
+    startGame();
+}
+
+function isValidNumber(num) {
+    return !isNaN(num) || num > 0;
+}
+
+function handleMoveNext(event) {
+    if (event.key == "Enter" || event.key == "Tab") {
+        console.log(event.target.nextElementSibling.focus());
+    }
+}
 
 function handleButton(event) {
     const index = cells.indexOf(event.target);
@@ -156,10 +290,6 @@ function handleButton(event) {
 }
 
 function handleDblClick(event) {
-    if (typeClick === "flag") {
-        return;
-    }
-    
     const index = cells.indexOf(event.target);
     const column = index % WIDTH;
     const row = Math.floor(index / WIDTH);
@@ -175,61 +305,58 @@ function handleClick(event) {
     const column = index % WIDTH;
     const row = Math.floor(index / WIDTH);
 
-   // console.log(2, cells);
-    // console.log(index);
-    // console.log(cells[index]);
     if (typeClick === "cell") {
         open(row, column);
     } else {
-        toggleFlag(index);
-        if (toggleType.disabled) {
+        if (!toggleFlag(index) || typeClick == 'flags') {
             return;
         }
         typeClick = 'cell';
-        toggleType.classList.replace('flag_green', 'flag');
+        toggleTypeOne.classList.replace('flag-one-press', 'flag-one');
     }
 }
 
 function renderField(closeCount, sizeCell) {
-    smile.classList.replace(smile.classList[0], 'smile');
+    smile.classList.replace(smile.classList[0], 'smile');;
+
     field.innerHTML = '<button></button>'.repeat(closeCount);
+    field.style.gridTemplateColumns = `repeat(${WIDTH}, 1fr`;
+
+    container.style.minWidth = `${WIDTH * 2.5 + 2}rem`;
+
     cells = [...field.children];
 
     cells.forEach(cell => {
         cell.classList.add('cell');
         cell.classList.add(`size-${sizeCell}`);
         cell.classList.add('cell_closed');
+        
         cell.dataset.flag = 0;
         cell.dataset.isOpen = 0;
         cell.dataset.countMines = 0;
-        // cell.innerHTML = '<img class="count_mines" src="img/closed_cell.png" alt="count mines">';
+        
         cell.addEventListener('mousedown', handleButton);
         cell.addEventListener('click', handleClick);
         cell.addEventListener('dblclick', handleDblClick);
     })    
 }
 
-function renderSize(sizeCell) {
-    field.style.gridTemplateColumns = `repeat(${WIDTH}, ${sizeCell}rem)`;
-    smile.classList.replace(smile.classList[1], `size-${2 * sizeCell}`);
-    toggleType.classList.replace(toggleType.classList[2], `size-${sizeCell + 1}`);
-    
-    cells.forEach(cell => cell.classList.replace(cell.classList[1], `size-${sizeCell}`));
-    texts.forEach(text => text.classList.replace(text.classList[0], `text-${sizeCell}`));
-    
-    document.querySelectorAll('.digit').forEach(img => img.classList.replace(img.classList[2], `digit-size-${sizeCell}`));
-    document.querySelector('.game').style.width = `${WIDTH * sizeCell}rem`;
-    document.querySelector('.container').style.minWidth = `${WIDTH * sizeCell + 2}rem`;
-}
-
 function renderCounter(leftMines)
 {
-    const hundredths = Math.floor((leftMines % 1000) / 100);
-    const tenths = Math.floor(leftMines / 10);
+    let hundreds = Math.floor(leftMines / 100) % 10;
+
+    if (leftMines < 0) {
+        hundreds = 'minus';
+        leftMines = Math.abs(leftMines);
+    } else {
+        hundreds = Math.floor(leftMines / 100) % 10;
+    }
+    
+    const tenths = Math.floor(leftMines / 10) % 10;
     const units = leftMines % 10;
     
-    if (hundredths != timer100.classList[1].slice(-1)) {
-        timer100.classList.replace(timer100.classList[1], `digit_${hundredths}`);
+    if (hundreds != counter100.classList[1].slice(-1)) {
+        counter100.classList.replace(counter100.classList[1], `digit_${hundreds}`);
     }
     
     if (tenths != counter10.classList[1].slice(-1)) {
@@ -241,12 +368,12 @@ function renderCounter(leftMines)
 
 function renderTimer(value)
 {
-    const hundredths = Math.floor((value % 1000) / 100);
-    const tenths = Math.floor((value % 100) / 10);
+    const hundreds = Math.floor(value / 100) % 10;
+    const tenths = Math.floor(value / 10) % 10;
     const units = value % 10;
     
-    if (hundredths != timer100.classList[1].slice(-1)) {
-        timer100.classList.replace(timer100.classList[1], `digit_${hundredths}`);
+    if (hundreds != timer100.classList[1].slice(-1)) {
+        timer100.classList.replace(timer100.classList[1], `digit_${hundreds}`);
     }
     
     if (tenths != timer10.classList[1].slice(-1)) {
@@ -292,19 +419,6 @@ function getFlagsCount(row, column) {
 }
 
 function open(row, column) {
-    if (!is_start) {
-        is_start = true;
-        while (isBomb(row, column)) {
-            bombs = [...Array(closeCount).keys()]
-            .sort(() => Math.random() - 0.5)
-            .slice(0, BOMBS_COUNT);
-        }
-
-        let timerGame = 0;
-        timerId = setInterval(() => renderTimer(timerGame++), 1000);
-    }
-    
-
     if (!isValid(row, column)) {
         return;
     }
@@ -318,8 +432,18 @@ function open(row, column) {
     
     cell.dataset.isOpen = 1;
     
+    if (!is_start) {
+        is_start = true;
+        if (isBomb(row, column)) {
+            bombs = bombs.filter((bomb) => bomb != index);
+            installationMines(1);
+        }
+        
+        let timerGame = 0;
+        timerId = setInterval(() => renderTimer(timerGame++), 1000);
+    }
+    
     if (isBomb(row, column)) {
-        // cell.innerHTML = '<img class="count_mines" src="img/detonated_mine.png" alt="count mines">';
         cell.classList.replace(cell.classList[2], 'mine_detonated');
         cell.dataset.flag = 5;
         endGame(false);
@@ -335,11 +459,9 @@ function open(row, column) {
 
     if (count > 0) {
         cell.dataset.countMines = count;
-        // cell.innerHTML=`<img class="count_mines" src="img/${imgCount[count]}_mines.png" alt="count mines">`;
         cell.classList.replace(cell.classList[2], `cell_${count}`);
         return;
     }
-    // cell.innerHTML = '<img class="count_mines" src="img/opened_cell.png" alt="count mines">';
     cell.classList.replace(cell.classList[2], 'cell_opened');
     openAround(row, column);
 }
@@ -372,12 +494,15 @@ function unbindHandler() {
 
 function endGame(is_win) {
     unbindHandler();
-    
+    hintBtn.disabled = true;
+    toggleTypeOne.disabled = true;
+    toggleTypeSet.disabled = true;
+    resetType();
+
     for (const bomb of bombs) {
         if (cells[bomb].dataset.flag == 1 || cells[bomb].dataset.flag == 5) {
             continue;
         } else {
-            // cells[bomb].innerHTML = '<img class="count_mines" src="img/mine.png" alt="count mines">';
             cells[bomb].classList.replace(cells[bomb].classList[2], 'mine');
         }
     }
@@ -386,11 +511,9 @@ function endGame(is_win) {
     for (const flag of flags) {
         const index_flag = cells.indexOf(flag);
         if (!bombs.includes(index_flag)) {
-            // flag.innerHTML = '<img class="count_mines" src="img/error_mine.png" alt="count mines">';
             flag.classList.replace(flag.classList[2], 'mine_error');
         }
     }
-
 
     if (is_win) {
         smile.classList.replace(smile.classList[0], 'smile_win');
